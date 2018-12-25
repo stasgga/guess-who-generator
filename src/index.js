20a9b855d3875debc7f7d6904afbaaa300d65017
stasgga <stsggrn25@gmail.com>
2018-12-25 02:02:49 +0000

    seinfeld

--- src/index.js
+++ src/index.js
@@ -1,1 +1,63 @@
-alert("hello");
+import React from "react";
+import { render } from "react-dom";
+
+const cards = [
+  "George",
+  "Jerry",
+  "Kramer",
+  "Elaine",
+  "Morty Seinfeld",
+  "Soup Nazi",
+  "Babu Bhatt",
+  "Jean Paul",
+  "Jackie Chiles",
+  "Lloyd Braun",
+  "Nina West",
+  "Poppie",
+  "Mr Kruger",
+  "Susan Ross",
+  "Tony",
+  "Mendelbaum",
+  "Tim Whatley",
+  "The Wiz",
+  "Newman",
+  "David Puddy",
+  "Mr Ross",
+  "Mrs Ross",
+  "Helen Seinfeld",
+  "Uncle Leo",
+  "Estelle",
+  "Frank",
+  "Ruthie Cohen"
+];
+
+const Card = ({ name }) => (
+  <div
+    className="card"
+    style={{
+      backgroundImage: `url(http://localhost:5000/img/${name
+        .replace(" ", "-")
+        .toLowerCase()}.jpg)`
+    }}
+  >
+    {/* <div
+      className="img"
+      style={{
+        backgroundImage: `url(http://localhost:5000/img/${name
+          .replace(" ", "-")
+          .toLowerCase()}.jpg)`
+      }}
+    /> */}
+    <span>{name}</span>
+  </div>
+);
+
+const App = () => (
+  <div>
+    {cards.map(card => (
+      <Card key={card} name={card} />
+    ))}
+  </div>
+);
+
+render(<App />, document.getElementById("main"));
